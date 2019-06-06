6ba79e931fe62fa0a34f5aab152ac100cf34f5b0
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-06-06 11:46:04 +0200

    fixes

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: anben <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:20:31 by anben             #+#    #+#             */
-/*   Updated: 2019/06/06 10:48:18 by anben            ###   ########.fr       */
+/*   Updated: 2019/06/06 11:45:19 by anben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	i = 0;
 	st1 = (char *)s;
 	str = (char *)ft_memalloc(sizeof(char) * (ft_strlen(st1)) + 1);
-	if (str == NULL || st1 == NULL)
+	if (str == NULL)
+		return (NULL);
+	if (str == NULL && st1 == NULL)
 		return (NULL);
 	n = ft_strlen(st1);
-	//if (!(str = (ft_strnew(n))))
-		//return (NULL);
 	while (st1[i])
 	{
 		str[i] = f(i, st1[i]);
 		i++;
-		st1++;
 	}
+	str[i] = '\0';
 	return (str);
 }
