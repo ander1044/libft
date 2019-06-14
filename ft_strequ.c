a343dd678444655ace34d18a6b0ae43e741c5e76
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-06-14 13:25:34 +0200

    Fixes again

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 11:20:57 by event             #+#    #+#             */
-/*   Updated: 2019/06/07 14:09:46 by anben            ###   ########.fr       */
+/*   Updated: 2019/06/14 13:20:54 by anben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,17 @@ int	ft_strequ(char const *s1, char const *s2)
 	char	*st1;
 	char	*st2;
 	int		i;
-
+	
 	st1 = (char *)s1;
 	st2 = (char *)s2;
 	i = 0;
 	if (st1 == NULL)
 		return (0);
-	while (st1[i])
+	while (st1[i] == st2[i] && st1[i] && st2[i])
 	{
-		if (st1[i] == st2[i])
-		{
-			i++;
-		}
-		else
-		{
-			return (0);
-		}
+		i++;
 	}
-	if (st1 == st2 && st2 != '\0')
+	if (st1[i] == st2[i])
 		return (1);
 	return (0);
 }
